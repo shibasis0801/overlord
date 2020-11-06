a7d2698a7b0405014cc87c32e82a3c201e80443e
Shibasis <shibasis0801@gmail.com>
2020-11-07 00:39:00 +0530

    threaded sum working with terrible performance, 20 times slower

--- cpp/code/include/threading.hpp
+++ cpp/code/include/threading.hpp
@@ -16,7 +16,7 @@ llong calculateSum(
 ) {
     llong sum = 0;
     traverse(i, start, end) {
-        sum += numbers[i];
+        sum += numbers[i] * numbers[i];
     }
     return sum;
 }
@@ -24,16 +24,26 @@ llong calculateSum(
 llong calculateThreadedSum(
         const vector<int> &numbers
 ) {
-    auto n_proc = thread::hardware_concurrency();
+    auto n_proc = thread::hardware_concurrency() - 1;
     auto size = numbers.size();
 
     int chunk_size = (int)(size / n_proc) + 1;
     llong sum = 0;
 
+    vector<thread> processors(n_proc);
+    vector<llong> sums(n_proc);
+
     repeat(i, n_proc) {
         auto start = i * chunk_size;
         auto end = min(start + chunk_size, size);
-        sum += calculateSum(numbers, start, end);
+        processors[i] = thread([&, i, start, end](llong &acc) {
+           acc = calculateSum(numbers, start, end);
+        }, std::ref(sums[i]));
+    }
+
+    repeat(i, n_proc) {
+        processors[i].join();
+        sum += sums[i];
     }
 
     return sum;
--- cpp/code/src/app.cpp
+++ cpp/code/src/app.cpp
@@ -12,12 +12,22 @@ using namespace ovd;
 using namespace std;
 using namespace std::chrono;
 
+inline decltype(auto) getEnd(time_point<steady_clock> start) {
+    return duration_cast<nanoseconds>(high_resolution_clock::now() - start).count();
+}
+
 int main () {
+    auto elements = utils::createVector(1, 10000);
 
-    auto elements = utils::createVector(1, 100);
+    auto startNormal = high_resolution_clock::now();
     auto sumNormal = calculateSum(elements, 0, elements.size());
-    auto sumThreaded = calculateThreadedSum(elements);
+    auto timeNormal = getEnd(startNormal);
 
-    print("Sum should be ", sumNormal, " sumThreaded is ", sumThreaded, '\n');
+    auto startThreaded = high_resolution_clock::now();
+    auto sumThreaded = calculateThreadedSum(elements);
+    auto timeThreaded = getEnd(startThreaded);
+
+    print("sumNormal: ", sumNormal, ", time: ", timeNormal, '\n');
+    print("sumThreaded: ", sumThreaded, ", time: ", timeThreaded, '\n');
     return 0;
 }
