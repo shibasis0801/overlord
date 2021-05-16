edd029901b6dcca4397c734bfc8b653bdd7bd13c
Shibasis <shibasis0801@gmail.com>
2021-05-16 19:47:17 +0530

    changes

--- app/website/src/ui/components/apps/WebcamTester.tsx
+++ app/website/src/ui/components/apps/WebcamTester.tsx
@@ -12,7 +12,21 @@ export class WebcamTester extends React.Component<Props, State> {
     render() {
         return (
             <div>
-                <Webcam />
+                <Webcam
+                    audio={false}
+                    screenshotFormat={"image/jpeg"}
+                    videoConstraints={{
+                        width: 1280,
+                        height: 720,
+                        facingMode: "user"
+                    }}
+                    minScreenshotWidth={800}
+                    minScreenshotHeight={800}
+                    screenshotQuality={1}
+                    forceScreenshotSourceSize={false}
+                    mirrored={true}
+
+                />
             </div>
         );
     };
